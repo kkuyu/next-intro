f4add725a0f08f3d7938e8f2611f9147dc5c6566
kkuyu <devils67@naver.com>
2022-05-19 12:07:07 +0900

    CSS Modules

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import styles from "./NavBar.module.css";
+
 function NavBar() {
   const router = useRouter();
 
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link href="/" passHref>
-        <a style={{ color: router.pathname === "/" ? "red" : "black" }}>home</a>
+        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>home</a>
       </Link>
       <Link href="/about" passHref>
-        <a style={{ color: router.pathname === "/about" ? "red" : "black" }}>about</a>
+        <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>about</a>
       </Link>
     </nav>
   );
--- components/TestButton.js
+++ components/TestButton.js
@@ -1,13 +1,11 @@
-import React from "react";
+import { forwardRef } from "react";
 
-const TestButton = React.forwardRef(({ onClick, href, text }, ref) => {
+const TestButton = ({ onClick, href, text }, ref) => {
   return (
     <a href={href} onClick={onClick} ref={ref}>
       {text}
     </a>
   );
-});
+};
 
-TestButton.displayName = "TestButton";
-
-export default TestButton;
+export default forwardRef(TestButton);
