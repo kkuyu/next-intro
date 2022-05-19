52875057433d481a10765122c535e59228c1ce56
kkuyu <devils67@naver.com>
2022-05-19 12:14:39 +0900

    Styles JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,19 +1,33 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import styles from "./NavBar.module.css";
-
 function NavBar() {
   const router = useRouter();
 
   return (
-    <nav className={styles.nav}>
+    <nav className="nav">
       <Link href="/" passHref>
-        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>home</a>
+        <a className={"link " + (router.pathname === "/" ? "active" : "")}>home</a>
       </Link>
       <Link href="/about" passHref>
-        <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>about</a>
+        <a className={["link", router.pathname === "/about" ? "active" : ""].join(" ")}>about</a>
       </Link>
+      <style jsx>{`
+        .nav {
+          position: fixed;
+          top: 0;
+          right: 0;
+        }
+
+        .link {
+          padding: 0 4px;
+          text-transform: uppercase;
+        }
+
+        .active {
+          color: tomato;
+        }
+      `}</style>
     </nav>
   );
 }
