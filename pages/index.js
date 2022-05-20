11b3ee611d7aae1678295b64076117364c693973
kkuyu <devils67@naver.com>
2022-05-20 12:31:50 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,53 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
+
 import Seo from "../components/Seo";
+import styles from "../styles/Home.module.css";
 
 export default function Home({ movies }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Seo title="Home" />
       {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" width={500} height={750} />
-          <h4>{movie.original_title}</h4>
+        <div onClick={() => onClick(movie.id, movie.original_title)} className={styles.movie} key={movie.id}>
+          <div className={styles.thumb}>
+            <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" width={500} height={750} layout="responsive" />
+          </div>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
-      <style jsx>{`
-        .container {
-          display: grid;
-          grid-template-columns: 1fr 1fr;
-          padding: 20px;
-          gap: 20px;
-        }
-        .movie {
-          cursor: pointer;
-        }
-        .movie img {
-          max-width: 100%;
-          border-radius: 12px;
-          transition: transform 0.2s ease-in-out;
-          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-        }
-        .movie:hover img {
-          transform: scale(1.05) translateY(-10px);
-        }
-        .movie h4 {
-          font-size: 18px;
-          text-align: center;
-        }
-      `}</style>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  const { results } = await (await fetch(`${context.req.headers.referer}/api/movies`)).json();
+  const response = await fetch(`http://${context.req.headers.host}/api/movies`);
+  const { results } = await response.json();
   return {
     props: { movies: results },
   };
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router";
 
-export default function MoviesDetail() {
+export default function Detail() {
   const router = useRouter();
-  console.log(router.query.id);
-  return <div>MoviesDetail: {router.query.id}</div>;
+  return (
+    <div>
+      <h4>{router.query.title || "Loading..."}</h4>
+    </div>
+  );
 }
